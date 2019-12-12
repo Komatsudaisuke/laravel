ede12935f643614d89af0a76b4336d4951b564fc
”小松大介” <pocopeco.4@gmail.com>
2019-12-12 11:37:49 +0000

    laravel12

--- routes/web.php
+++ routes/web.php
@@ -23,4 +23,28 @@ Route::group(['prefix' => 'admin'], function() {
     Route::get('news/create', 'Admin\NewsController@add');
     Route::get('profile/create', 'Admin\ProfileController@add');
     Route::get('profile/edit', 'Admin\ProfileController@edit');
+    Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
+    Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
+    
 });
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
+    Route::get('news/create', 'Admin\NewsController@add');
+    Route::post('news/create', 'Admin\NewsController@create');
+});
+
+
+
+//URLに対して動かすプログラム（アクション）を定義するところ。扉というイメージ
+
+Route::group(['prefix' => 'admin'], function() {//adminでまとめる。
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
+    Route::post('news/create', 'Admin\NewsController@create')->middleware('auth');
+    Route::get('news', 'Admin\NewsController@index')->middleware('auth'); // 追記
+    Route::get('news/edit', 'Admin\NewsController@edit')->middleware('auth'); // 追記
+    Route::post('news/edit', 'Admin\NewsController@update')->middleware('auth');
+   Route::get('news/delete', 'Admin\NewsController@delete')->middleware('auth'); 
+});//URL、admin/news/deleteでアクション（Admin\NewsController@delete）する
